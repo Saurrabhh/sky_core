a9ebe61ad6e0ad4d11522e36e69f0afcbc5dfdd7
Saurrabhh <saurabhkyadav99@gmail.com>
2026-05-02 03:33:28 +0530

    feat: add dynamic theme switching and foundation details to example app

--- sky_design_system/example/lib/main.dart
+++ sky_design_system/example/lib/main.dart
@@ -5,8 +5,44 @@ import 'package:sky_design_system/sky_design_system.dart';
 
 import 'main.directories.g.dart';
 
+import 'foundations_screen.dart';
+
 void main() {
-  runApp(const WidgetbookApp());
+  runApp(const ShowcaseApp());
+}
+
+class ShowcaseApp extends StatefulWidget {
+  const ShowcaseApp({super.key});
+
+  @override
+  State<ShowcaseApp> createState() => _ShowcaseAppState();
+}
+
+class _ShowcaseAppState extends State<ShowcaseApp> {
+  final ValueNotifier<ThemeMode> _themeMode = ValueNotifier(ThemeMode.light);
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder<ThemeMode>(
+      valueListenable: _themeMode,
+      builder: (context, mode, child) {
+        return MaterialApp(
+          title: 'Sky Design System Showcase',
+          debugShowCheckedModeBanner: false,
+          theme: AppTheme.light(),
+          darkTheme: AppTheme.dark(),
+          themeMode: mode,
+          home: FoundationsScreen(themeMode: _themeMode),
+        );
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _themeMode.dispose();
+    super.dispose();
+  }
 }
 
 @widgetbook.App()
