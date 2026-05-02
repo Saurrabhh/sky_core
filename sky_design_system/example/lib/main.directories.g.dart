d778ff5a06945c82d71dea70d481bf2ce0ef49a3
Saurrabhh <saurabhkyadav99@gmail.com>
2026-05-02 06:23:55 +0530

    feat(example): create navigation organism use cases

--- sky_design_system/example/lib/main.directories.g.dart
+++ sky_design_system/example/lib/main.directories.g.dart
@@ -14,6 +14,16 @@ import 'package:example/components/atoms/app_button_use_case.dart'
     as _example_components_atoms_app_button_use_case;
 import 'package:example/components/atoms/app_text_use_case.dart'
     as _example_components_atoms_app_text_use_case;
+import 'package:example/components/atoms/indicators_use_cases.dart'
+    as _example_components_atoms_indicators_use_cases;
+import 'package:example/components/atoms/inputs_use_cases.dart'
+    as _example_components_atoms_inputs_use_cases;
+import 'package:example/components/molecules/feedback_molecules_use_cases.dart'
+    as _example_components_molecules_feedback_molecules_use_cases;
+import 'package:example/components/molecules/layout_molecules_use_cases.dart'
+    as _example_components_molecules_layout_molecules_use_cases;
+import 'package:example/components/organisms/navigation_use_cases.dart'
+    as _example_components_organisms_navigation_use_cases;
 import 'package:example/foundations/colors_use_case.dart'
     as _example_foundations_colors_use_case;
 import 'package:example/foundations/radius_use_case.dart'
@@ -32,12 +42,82 @@ final directories = <_widgetbook.WidgetbookNode>[
         name: 'atoms',
         children: [
           _widgetbook.WidgetbookComponent(
+            name: 'AppAvatar',
+            useCases: [
+              _widgetbook.WidgetbookUseCase(
+                name: 'Default',
+                builder: _example_components_atoms_indicators_use_cases
+                    .appAvatarUseCase,
+              ),
+            ],
+          ),
+          _widgetbook.WidgetbookComponent(
+            name: 'AppBadge',
+            useCases: [
+              _widgetbook.WidgetbookUseCase(
+                name: 'Default',
+                builder: _example_components_atoms_indicators_use_cases
+                    .appBadgeUseCase,
+              ),
+            ],
+          ),
+          _widgetbook.WidgetbookComponent(
             name: 'AppButton',
             useCases: [
               _widgetbook.WidgetbookUseCase(
                 name: 'Variants',
                 builder: _example_components_atoms_app_button_use_case
                     .appButtonVariantsUseCase,
+              ),
+            ],
+          ),
+          _widgetbook.WidgetbookComponent(
+            name: 'AppCheckbox',
+            useCases: [
+              _widgetbook.WidgetbookUseCase(
+                name: 'Default',
+                builder: _example_components_atoms_inputs_use_cases
+                    .appCheckboxUseCase,
+              ),
+            ],
+          ),
+          _widgetbook.WidgetbookComponent(
+            name: 'AppProgressIndicator',
+            useCases: [
+              _widgetbook.WidgetbookUseCase(
+                name: 'Circular & Linear',
+                builder: _example_components_atoms_indicators_use_cases
+                    .appProgressIndicatorUseCase,
+              ),
+            ],
+          ),
+          _widgetbook.WidgetbookComponent(
+            name: 'AppRadioButton',
+            useCases: [
+              _widgetbook.WidgetbookUseCase(
+                name: 'Default',
+                builder: _example_components_atoms_inputs_use_cases
+                    .appRadioButtonUseCase,
+              ),
+            ],
+          ),
+          _widgetbook.WidgetbookComponent(
+            name: 'AppSlider',
+            useCases: [
+              _widgetbook.WidgetbookUseCase(
+                name: 'Default',
+                builder:
+                    _example_components_atoms_inputs_use_cases.appSliderUseCase,
+              ),
+            ],
+          ),
+          _widgetbook.WidgetbookComponent(
+            name: 'AppSwitch',
+            useCases: [
+              _widgetbook.WidgetbookUseCase(
+                name: 'Default',
+                builder:
+                    _example_components_atoms_inputs_use_cases.appSwitchUseCase,
               ),
             ],
           ),
@@ -53,6 +133,101 @@ final directories = <_widgetbook.WidgetbookNode>[
                 name: 'Scale',
                 builder: _example_components_atoms_app_text_use_case
                     .appTextScaleUseCase,
+              ),
+            ],
+          ),
+          _widgetbook.WidgetbookComponent(
+            name: 'AppTextField',
+            useCases: [
+              _widgetbook.WidgetbookUseCase(
+                name: 'Default',
+                builder: _example_components_atoms_inputs_use_cases
+                    .appTextFieldUseCase,
+              ),
+            ],
+          ),
+        ],
+      ),
+      _widgetbook.WidgetbookFolder(
+        name: 'molecules',
+        children: [
+          _widgetbook.WidgetbookComponent(
+            name: 'AppCard',
+            useCases: [
+              _widgetbook.WidgetbookUseCase(
+                name: 'Outlined & Filled',
+                builder:
+                    _example_components_molecules_layout_molecules_use_cases
+                        .appCardUseCase,
+              ),
+            ],
+          ),
+          _widgetbook.WidgetbookComponent(
+            name: 'AppEmptyState',
+            useCases: [
+              _widgetbook.WidgetbookUseCase(
+                name: 'Default',
+                builder:
+                    _example_components_molecules_feedback_molecules_use_cases
+                        .appEmptyStateUseCase,
+              ),
+            ],
+          ),
+          _widgetbook.WidgetbookComponent(
+            name: 'AppListItem',
+            useCases: [
+              _widgetbook.WidgetbookUseCase(
+                name: 'Default',
+                builder:
+                    _example_components_molecules_layout_molecules_use_cases
+                        .appListItemUseCase,
+              ),
+            ],
+          ),
+          _widgetbook.WidgetbookComponent(
+            name: 'AppSearchBar',
+            useCases: [
+              _widgetbook.WidgetbookUseCase(
+                name: 'Default',
+                builder:
+                    _example_components_molecules_layout_molecules_use_cases
+                        .appSearchBarUseCase,
+              ),
+            ],
+          ),
+          _widgetbook.WidgetbookComponent(
+            name: 'AppTooltip',
+            useCases: [
+              _widgetbook.WidgetbookUseCase(
+                name: 'Default',
+                builder:
+                    _example_components_molecules_feedback_molecules_use_cases
+                        .appTooltipUseCase,
+              ),
+            ],
+          ),
+        ],
+      ),
+      _widgetbook.WidgetbookFolder(
+        name: 'organisms',
+        children: [
+          _widgetbook.WidgetbookComponent(
+            name: 'AppNavigationBar',
+            useCases: [
+              _widgetbook.WidgetbookUseCase(
+                name: 'Default',
+                builder: _example_components_organisms_navigation_use_cases
+                    .appNavigationBarUseCase,
+              ),
+            ],
+          ),
+          _widgetbook.WidgetbookComponent(
+            name: 'AppTabBar',
+            useCases: [
+              _widgetbook.WidgetbookUseCase(
+                name: 'Default',
+                builder: _example_components_organisms_navigation_use_cases
+                    .appTabBarUseCase,
               ),
             ],
           ),
