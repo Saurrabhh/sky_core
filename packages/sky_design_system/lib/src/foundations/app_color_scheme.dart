89c93191d8b0f58ca76cd3671abba621004a7050
Saurrabhh <saurabhkyadav99@gmail.com>
2026-05-22 01:20:08 +0530

    add app_color_scheme

---
+++ packages/sky_design_system/lib/src/foundations/app_color_scheme.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+class AppColorScheme {
+  const AppColorScheme({
+    required this.surface,
+    required this.surfaceDim,
+    required this.surfaceBright,
+    required this.surfaceContainerLowest,
+    required this.surfaceContainerLow,
+    required this.surfaceContainer,
+    required this.surfaceContainerHigh,
+    required this.surfaceContainerHighest,
+    required this.onSurface,
+    required this.onSurfaceVariant,
+    required this.inverseSurface,
+    required this.inverseOnSurface,
+    required this.outline,
+    required this.outlineVariant,
+    required this.surfaceTint,
+    required this.primary,
+    required this.onPrimary,
+    required this.primaryContainer,
+    required this.onPrimaryContainer,
+    required this.inversePrimary,
+    required this.secondary,
+    required this.onSecondary,
+    required this.secondaryContainer,
+    required this.onSecondaryContainer,
+    required this.tertiary,
+    required this.onTertiary,
+    required this.tertiaryContainer,
+    required this.onTertiaryContainer,
+    required this.error,
+    required this.onError,
+    required this.errorContainer,
+    required this.onErrorContainer,
+    required this.primaryFixed,
+    required this.primaryFixedDim,
+    required this.onPrimaryFixed,
+    required this.onPrimaryFixedVariant,
+    required this.secondaryFixed,
+    required this.secondaryFixedDim,
+    required this.onSecondaryFixed,
+    required this.onSecondaryFixedVariant,
+    required this.tertiaryFixed,
+    required this.tertiaryFixedDim,
+    required this.onTertiaryFixed,
+    required this.onTertiaryFixedVariant,
+    required this.background,
+    required this.onBackground,
+    required this.surfaceVariant,
+  });
+
+  final Color surface;
+  final Color surfaceDim;
+  final Color surfaceBright;
+  final Color surfaceContainerLowest;
+  final Color surfaceContainerLow;
+  final Color surfaceContainer;
+  final Color surfaceContainerHigh;
+  final Color surfaceContainerHighest;
+  final Color onSurface;
+  final Color onSurfaceVariant;
+  final Color inverseSurface;
+  final Color inverseOnSurface;
+  final Color outline;
+  final Color outlineVariant;
+  final Color surfaceTint;
+  final Color primary;
+  final Color onPrimary;
+  final Color primaryContainer;
+  final Color onPrimaryContainer;
+  final Color inversePrimary;
+  final Color secondary;
+  final Color onSecondary;
+  final Color secondaryContainer;
+  final Color onSecondaryContainer;
+  final Color tertiary;
+  final Color onTertiary;
+  final Color tertiaryContainer;
+  final Color onTertiaryContainer;
+  final Color error;
+  final Color onError;
+  final Color errorContainer;
+  final Color onErrorContainer;
+  final Color primaryFixed;
+  final Color primaryFixedDim;
+  final Color onPrimaryFixed;
+  final Color onPrimaryFixedVariant;
+  final Color secondaryFixed;
+  final Color secondaryFixedDim;
+  final Color onSecondaryFixed;
+  final Color onSecondaryFixedVariant;
+  final Color tertiaryFixed;
+  final Color tertiaryFixedDim;
+  final Color onTertiaryFixed;
+  final Color onTertiaryFixedVariant;
+  final Color background;
+  final Color onBackground;
+  final Color surfaceVariant;
+}
