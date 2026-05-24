b7bea6eb938e5b2fab418bd71f5b89a80131b025
Saurrabhh <saurabhkyadav99@gmail.com>
2026-05-25 03:43:01 +0530

    ref

--- packages/sky_network/lib/src/factory/dio_factory.dart
+++ packages/sky_network/lib/src/factory/dio_factory.dart
@@ -3,25 +3,14 @@ import 'package:flutter/foundation.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 import 'package:sky_network/src/options/network_options.dart';
 
-// The interface pattern is intentionally chosen here for DI mockability.
-// ignore: one_member_abstracts
 abstract interface class DioFactory {
-  /// Creates and configures a [Dio] instance.
-  ///
-  /// Takes non-nullable [options] and an optional list of [interceptors]
-  /// to add to the created client.
   Dio create({
     required NetworkOptions options,
     List<Interceptor> interceptors,
   });
 }
 
-/// {@template dio_factory_impl}
-/// Concrete implementation of [DioFactory] configuring standard headers,
-/// connection timeouts, and registering [PrettyDioLogger] for telemetry.
-/// {@endtemplate}
 class DioFactoryImpl implements DioFactory {
-  /// {@macro dio_factory_impl}
   const DioFactoryImpl();
 
   @override
@@ -38,7 +27,6 @@ class DioFactoryImpl implements DioFactory {
       ..sendTimeout = options.sendTimeout
       ..headers = options.headers ?? {};
 
-    // 1. Add standard pretty logger in debug mode only for formatting.
     if (kDebugMode) {
       dio.interceptors.add(
         PrettyDioLogger(
