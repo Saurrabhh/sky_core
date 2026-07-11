e441fedaa2b9e8b8d3886036661eb743a4b671c3
Saurrabhh <saurabhkyadav99@gmail.com>
2026-07-12 00:01:29 +0530

    fix(sky_bloc): add helper exports

--- packages/sky_bloc/lib/sky_bloc.dart
+++ packages/sky_bloc/lib/sky_bloc.dart
@@ -1,9 +1,12 @@
 /// Core BLoC components, base classes, state management, and telemetry.
 library;
 
+export 'package:bloc/bloc.dart';
+export 'package:flutter_bloc/flutter_bloc.dart';
+
 export 'src/base/bloc/base_bloc.dart';
-export 'src/base/cubit/base_cubit.dart';
 export 'src/base/bloc/base_event.dart';
 export 'src/base/bloc/base_state.dart';
+export 'src/base/cubit/base_cubit.dart';
 export 'src/base/page/base_page.dart';
 export 'src/observer/telemetry_bloc_observer.dart';
