f5844afffc6001e33153f12eea62a8c2139fc21b
Saurrabhh <saurabhkyadav99@gmail.com>
2026-07-19 18:05:18 +0530

    fix(sky_router): updated exports

--- packages/sky_router/lib/sky_router.dart
+++ packages/sky_router/lib/sky_router.dart
@@ -1,14 +1,4 @@
-export 'package:go_router/go_router.dart'
-    show
-        CustomTransitionPage,
-        GoException,
-        GoRoute,
-        GoRouter,
-        GoRouterState,
-        RouteBase,
-        ShellRoute,
-        StatefulShellBranch,
-        StatefulShellRoute;
+export 'package:go_router/go_router.dart';
 
 export 'src/custom_navigator_observer.dart';
 export 'src/go_router_refresh_stream.dart';
